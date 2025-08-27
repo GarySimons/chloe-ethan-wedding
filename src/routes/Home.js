d6b4b2dc0b9fa5c4d5303800b5a8dbb7e9ccc090
GarySimons <garycsimons@gmail.com>
2025-08-27 06:27:02 +0100

    scallop shell added

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import waxSeal from "../assets/wax-seal.png";
+import scallopShell from "../assets/scallop.png";
 import "./HomeStyles.scss";
 
 const Home = () => {
@@ -22,7 +23,7 @@ const Home = () => {
           <hr className="home-line" />
           <p className="home-address">ARUNDEL, WEST SUSSEX</p>
         </div>
-        <img src={waxSeal} alt="Wax Seal" className="wax-image" />
+        <img src={scallopShell} alt="Scallop Shell" className="wax-image" />
       </div>
 
       <Footer />
